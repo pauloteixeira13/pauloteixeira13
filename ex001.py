f5f9003762969c3645228b4d2d342a8d7744d731
pauloteixeira13 <120684545+pauloteixeira13@users.noreply.github.com>
2024-01-22 21:29:33 -0300

    Delete ex001.py

--- ex001.py
+++
@@ -1,9 +0,0 @@
-#primeiros trabalhos
-
-
-nome = input('Qual é o seu nome?')
-dia = int(input('Qual foi o dia do seu nascimento?'))
-mes = (input('Em qual mês você nasceu?'))
-ano = input('Em que ano você nasceu?')
-
-print('Olá!',nome.upper(), 'você nasceu no dia',dia,'de',mes.upper(),'do ano',ano,)
